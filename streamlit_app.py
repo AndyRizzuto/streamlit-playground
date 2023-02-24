c56700e26685822978bcb0aa7dd6c7aa62dda7e8
Riz <andyrizzuto@gmail.com>
2023-02-23 20:28:32 -0800

    feat: add df

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,10 @@ import pandas as pd
 import numpy as np
 
 st.title('My Health Metrics')
+
+df = pd.DataFrame({
+    'date': ["1/1/2023","1/3/2023","1/9/2023"],
+    'weight': [165,168,168.5]
+})
+
+st.write(df)
