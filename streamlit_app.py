43463abc8602b3979841f36c1f42b1bd3aeec8d2
Riz <andyrizzuto@gmail.com>
2023-03-01 16:33:00 -0800

    Feat: removed Apple Health data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,33 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from pydataset import data
+
+
+def pydata():
+    # Pydataset controls
+    selected_data = st.sidebar.selectbox('Select a dataset', data().dataset_id)
+    title_data = data()[ data()['dataset_id'] == selected_data]['title']
+
+    st.header('Datasets')
+    st.subheader('List of dataset')
+    with st.expander('Show list of dataset'):
+        st.write(data())
+
+    st.subheader(f'Selected data (`{selected_data}`)')
+    st.info(title_data)
+    st.write(data(selected_data))
+    return
 
 st.title('My Health Metrics')
+st.sidebar.text_input("Your name", key="name")
+greeting = "Hello "+ st.session_state.name
+st.write(greeting)
 
-df = pd.DataFrame({
-    'date': ["1/1/2023","1/3/2023","1/9/2023"],
-    'weight': [165,168,168.5]
-})
+#df =load_apple_health()
+pydata()
 
-st.write(df)
+# References
+# https://towardsdatascience.com/analyse-your-health-with-python-and-apple-health-11c12894aae2
+# https://github.com/dataprofessor/pydataset/
+# https://blog.streamlit.io/streamlit-firestore/
